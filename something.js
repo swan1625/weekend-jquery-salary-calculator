31d3a610e84911d0e7f9bf521df3228ecdaae2b0
Andy Swanson <swan1625@icloud.com>
2022-01-29 22:27:30 -0600

    delete button deleting itself

--- something.js
+++ something.js
@@ -1,37 +1,70 @@
 $( document ).ready ( readyNow );
+
+
+
+let employeeList = [];   //store employees object
+
+let total = 0; // store Monthly Total 
+
+
+
+
+
+
 function readyNow() {
 
     $( '#submitButton' ).on( 'click', addEmployee );
-
+    $( '#myTable' ).on('click', '#deleteButton',  excludeRow);
 }
 
-let employeeList = [];
+function excludeRow(){
+
+    console.log('this is', $(this));
+    $(this).remove();
+
+  }
 
 function addEmployee() {
 
-    const NewEmpObject = {
+    const NewEmpObject = {                       // creating a 
       FirstName: $('#FirstName').val(),
       lastName: $('#lastName').val(),
       Identity: $('#Identity').val(),
       Title:   $('#Title').val(),
-      Salary: $('#Salary').val()
+      Salary: Number($('#Salary').val())
     }
-    employeeList.push(NewEmpObject);
+
+    employeeList.push(NewEmpObject);   //pushing new employee to array 
 
 
-    $('#FirstName').val('');    //clearing input boxes
-    $('#lastName').val('');
-    $('#Identity').val('');
-    $('#Title').val('');
-    $('#Salary').val('');
     
  
     $("#myTable").append(`<tr><td>${NewEmpObject.FirstName}
     </td><td> ${NewEmpObject.lastName}
     </td><td> ${NewEmpObject.Identity} 
     </td><td> ${NewEmpObject.Title}
-    </td><td> ${NewEmpObject.Salary} </td></tr> `)
+    </td><td> ${NewEmpObject.Salary} 
+    </td><td>  <button id="deleteButton"> Delete </button>
+    </td></tr> `)
 
-    return true;
+    $('#FirstName').val('');    //clearing input boxes
+    $('#lastName').val('');
+    $('#Identity').val('');
+    $('#Title').val('');
+    $('#Salary').val('');
+
+    total += NewEmpObject.Salary;  // 
+    calc(total);   /// do i need this? 
+    console.log(employeeList);
+    return true;   /// do i need thus? 
   }
 
+  function calc(total) {
+      let monthlycost = ( total / 12 );
+      $('#span').empty(); 
+        $('#span').append(monthlycost.toFixed(2)); 
+
+      
+  }
+
+  
